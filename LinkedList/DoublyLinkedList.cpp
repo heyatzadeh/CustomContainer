7f5d95e861473a97f5aa7c1aee6a17b4627214e4
Hossein Heyatzadeh <h.heyatzadeh@gmail.com>
2023-09-23 18:47:40 +0330

    Added DoublyLinkedList class constructor and operators

--- LinkedList/DoublyLinkedList.cpp
+++ LinkedList/DoublyLinkedList.cpp
@@ -1,2 +1,69 @@
 #include "DoublyLinkedList.h"
 
+DoublyLinkedList::DoublyLinkedList(const int* input, int size)
+{
+    if (size < 1)
+    {
+        return;
+    }
+
+    first = new Node(nullptr, nullptr, input[0]);
+    this->size = size;
+    auto start{first};
+
+    for (int i = 1; i < size; ++i)
+    {
+        auto tempNode = new Node(start, nullptr, input[i]);
+        start->next = tempNode;
+        start = start->next;
+    }
+}
+
+DoublyLinkedList::DoublyLinkedList(std::vector<int> inputs) : DoublyLinkedList(inputs.data(), inputs.size())
+{
+}
+
+DoublyLinkedList::~DoublyLinkedList()
+{
+    auto node{first};
+
+    while (node)
+    {
+        first = first->next;
+        delete node;
+        node = first;
+    }
+}
+
+std::ostream& operator<<(std::ostream& out, const DoublyLinkedList& doublyLinkedList)
+{
+    out << '[';
+    auto node{doublyLinkedList.first};
+
+    while (node)
+    {
+        out << node->data << ' ';
+        node = node->next;
+    }
+    out << ']';
+    return out;
+}
+
+bool DoublyLinkedList::operator==(const DoublyLinkedList& other) const
+{
+    if (size != other.size)
+    {
+        return false;
+    }
+    auto firstNode{first}, secondNode{other.first};
+    while (firstNode && secondNode)
+    {
+        if (firstNode->data != secondNode->data)
+        {
+            return false;
+        }
+        firstNode = firstNode->next;
+        secondNode = secondNode->next;
+    }
+    return true;
+}
--- LinkedList/DoublyLinkedListTests.cpp
+++ LinkedList/DoublyLinkedListTests.cpp
@@ -1,6 +1,18 @@
 #include "DoublyLinkedList.h"
 #include <catch_amalgamated.hpp>
 
-TEST_CASE("")
+TEST_CASE("Constructor")
 {
+    SECTION("Can create Simple Node")
+    {
+        int inputs[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        DoublyLinkedList doublyLinkedList1(inputs, std::size(inputs));
+        DoublyLinkedList doublyLinkedList2({1, 2, 3, 4, 5, 6, 7, 8, 9, 10});
+        DoublyLinkedList doublyLinkedList3({1, 2, 3, 4, 5, 15, 7, 8, 9, 10});
+        DoublyLinkedList doublyLinkedList4({1, 2});
+        REQUIRE(doublyLinkedList1 == doublyLinkedList2);
+        REQUIRE(doublyLinkedList1 != doublyLinkedList3);
+        REQUIRE(doublyLinkedList2 != doublyLinkedList3);
+        REQUIRE(doublyLinkedList3 != doublyLinkedList4);
+    }
 }
